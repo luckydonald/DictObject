47d3252a16cb5df1bb9775072b9acf9e0752f12f
luckydonald <luckydonald@flutterb.at>
2015-08-14 00:01:04 +0200

    Still (x4) "Version Bump to 0.1.2". setup.py: appearently

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
 	'Programming Language :: Python',
 	'Programming Language :: Python :: 2',
 	'Programming Language :: Python :: 2.7',
-	'Programming Language :: Python :: 2.7.5',
 	# 'Programming Language :: Python :: 3',
 	# 'Programming Language :: Python :: 3.2',
 	# 'Programming Language :: Python :: 3.3',
