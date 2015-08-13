a9fda33738344f353871a96ac720caa622379a9c
luckydonald <luckydonald@flutterb.at>
2015-08-14 00:03:58 +0200

    Still (x4,5) "Version Bump to 0.1.2". setup.py: apparently python 2.7.5 is not allowed as classifier. Also I just hit submit instead of the 'correct typo' menu option. Shortcuts, Yeesh!

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
 	# 'Programming Language :: Python :: 3',
 	# 'Programming Language :: Python :: 3.2',
 	# 'Programming Language :: Python :: 3.3',
-	'Programming Language :: Python :: 3.4',
 	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.4',
 	'Topic :: Software Development :: Libraries'
 	],
 	# What does your project relate to?
